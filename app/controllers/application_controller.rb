d81590c0506e25634ab4a38a5ac0adfb62183083
harukicode0 <71533263+harukicode0@users.noreply.github.com>
2022-06-07 14:49:01 +0900

    新規登録、ログイン画面の表示

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,12 +1,19 @@
 class ApplicationController < ActionController::Base
   before_action :basic_auth
-
-
+  before_action :configure_permitted_parameters, if: :devise_controller?
+  
   private
 
   def basic_auth
     authenticate_or_request_with_http_basic do |username, password|
       username == ENV["BASIC_AUTH_USER"] && password == ENV["BASIC_AUTH_PASSWORD"]
     end
+  end
+
+  def configure_permitted_parameters  # メソッド名は慣習
+    # deviseのUserモデルにパラメーターを許可
+    devise_parameter_sanitizer.permit(:user, keys: [:nickname, 
+      :encrypted_password, :confirmed_password, :first_name, 
+      :last_name, :first_name_kana, :last_name_kana, :birthday])
   end
 end
