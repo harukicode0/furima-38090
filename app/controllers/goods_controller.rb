bbbfb49616ea621b5889966415aef02379592733
harukicode0 <71533263+harukicode0@users.noreply.github.com>
2022-06-08 18:54:23 +0900

    商品出品veiwの整理

--- app/controllers/goods_controller.rb
+++ app/controllers/goods_controller.rb
@@ -1,2 +1,29 @@
 class GoodsController < ApplicationController
+  def index
+
+  end
+
+  def new
+    @good = Good.new
+  end
+
+  def create
+    # @object = Object.new(params[:object])
+    # if @object.save
+    #   flash[:success] = "Object successfully created"
+    #   redirect_to @object
+    # else
+    #   flash[:error] = "Something went wrong"
+    #   render 'new'
+    # end
+  end
+  
+
+  def show
+  end
+
+  def edit
+    # @ = .find()
+  end
+  
 end
