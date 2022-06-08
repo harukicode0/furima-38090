cacbd48922b8587abd7b11c31376fd46e6460a12
harukicode0 <71533263+harukicode0@users.noreply.github.com>
2022-06-08 23:25:08 +0900

    priceのvalidateを修正、整数のみ受付

--- app/models/good.rb
+++ app/models/good.rb
@@ -3,7 +3,7 @@ class Good < ApplicationRecord
   with_options presence: true do
     validates :name
     validates :description
-    validates :price, numericality:{greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999}
+    validates :price, numericality:{only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999}
     with_options numericality: { other_than: 1, message: "can't be blank"} do
       validates :category_id
       validates :condition_id
