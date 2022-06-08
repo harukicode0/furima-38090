b504cf50bbee972be0926f43eaa4af6c5b31b4a2
harukicode0 <71533263+harukicode0@users.noreply.github.com>
2022-06-08 17:31:38 +0900

    商品テーブルのvalidatesの作成

--- app/models/good.rb
+++ app/models/good.rb
@@ -1,7 +1,19 @@
 class Good < ApplicationRecord
   # validates
+  with_options presence: true do
+    validates :name
+    validates :description
+    validates :price, numericality:{in: 300..9999999}
+    with_options numericality: { other_than: 1 } do
+      validates :category_id
+      validates :condition_id
+      validates :delivery_load_id
+      validates :delivery_day_id
+      validates :prefecture_id
+    end
+  end
   
-  
+
   # association
   belongs_to :user
 
