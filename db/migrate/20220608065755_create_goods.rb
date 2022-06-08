cd4a6624a9f46f256335fa4026a84cd0b26168ff
harukicode0 <71533263+harukicode0@users.noreply.github.com>
2022-06-08 17:18:09 +0900

    goodsテーブルをマイグレート

--- db/migrate/20220608065755_create_goods.rb
+++ db/migrate/20220608065755_create_goods.rb
@@ -3,13 +3,13 @@ class CreateGoods < ActiveRecord::Migration[6.0]
     create_table :goods do |t|
       t.string :name, null:false
       t.text :description, null:false
-      t.integer :price, null:false,
-      t.references :user, foreign_key: true
+      t.integer :price, null:false
       t.integer :category_id, null:false
       t.integer :condition_id, null:false
       t.integer :delivery_load_id, null:false
       t.integer :prefecture_id, null:false
       t.integer :delivery_day_id, null:false
+      t.references :user, foreign_key: true
       t.timestamps
     end
   end
