c85288c80872974b7a38b6065d7fafcc05b93091
harukicode0 <71533263+harukicode0@users.noreply.github.com>
2022-06-08 14:39:19 +0900

    validatesの設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,10 +4,22 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   
-  validates :first_name, presence: true
-  validates :first_name_kana, presence: true
-  validates :last_name, presence:true
-  validates :last_name_kana, presence:true
-  validates :nickname, presence:true
-  validates :birthday, presence:true
+  PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
+  validates_format_of :password, with: PASSWORD_REGEX, message: 'には英字と数字の両方を含めて設定してください'
+
+  Check_name_kanji_kana_hiragana_REGEX = /\A[ぁ-んァ-ヶ一-龥々ー]+\z/
+  Check_name_kana_REGEX = /\A[ァ-ヶー]+\z/
+
+  with_options presence: true do
+    with_options format:{with:Check_name_kanji_kana_hiragana_REGEX, message: "は全角入力でお願いします"} do
+      validates :first_name
+      validates :last_name
+    end
+    with_options format:{with:Check_name_kana_REGEX, message: "は全角カタカナで入力をお願いします"} do
+      validates :first_name_kana
+      validates :last_name_kana
+    end
+    validates :nickname
+    validates :birthday
+  end
 end
