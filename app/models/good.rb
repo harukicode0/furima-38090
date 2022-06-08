12e48ba32705db770409f937e11b2b2c4af3eb39
harukicode0 <71533263+harukicode0@users.noreply.github.com>
2022-06-08 18:08:01 +0900

    コントローラーの生成

--- app/models/good.rb
+++ app/models/good.rb
@@ -4,7 +4,7 @@ class Good < ApplicationRecord
     validates :name
     validates :description
     validates :price, numericality:{in: 300..9999999}
-    with_options numericality: { other_than: 1 } do
+    with_options numericality: { other_than: 1, message: "can't be blank"} do
       validates :category_id
       validates :condition_id
       validates :delivery_load_id
