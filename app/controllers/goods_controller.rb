d300f942a81ed6d09c907b661927a304e9eb88fc
harukicode0 <71533263+harukicode0@users.noreply.github.com>
2022-06-10 01:02:25 +0900

    商品編集機能実装

--- app/controllers/goods_controller.rb
+++ app/controllers/goods_controller.rb
@@ -1,5 +1,6 @@
 class GoodsController < ApplicationController
-  before_action :move_to_index, except: [:index,:show]  
+  before_action :move_to_index, except: [:index,:show] 
+  before_action :editor_is_correct_user, only: [:edit, :update]
 
   def index
     @goods = Good.includes(:user).order("created_at DESC")
@@ -27,6 +28,15 @@ class GoodsController < ApplicationController
     @good = Good.find(params[:id])
   end
 
+  def update
+    @good = Good.find(params[:id])
+    if @good.update(good_params)
+      redirect_to good_path
+    else
+      render 'edit'
+    end
+  end
+
   private
 
   def good_params
@@ -38,6 +48,12 @@ class GoodsController < ApplicationController
   def move_to_index
     unless user_signed_in?
       redirect_to new_user_session_path
+    end
+  end
+
+  def editor_is_correct_user
+    unless Good.find(params[:id]).user.id == current_user.id
+      redirect_to root_path
     end
   end
 end
