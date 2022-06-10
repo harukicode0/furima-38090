0ec8981141de4c55876de34c2347f3a1309f085f
harukicode0 <71533263+harukicode0@users.noreply.github.com>
2022-06-10 14:21:27 +0900

    修正

--- app/controllers/goods_controller.rb
+++ app/controllers/goods_controller.rb
@@ -1,6 +1,6 @@
 class GoodsController < ApplicationController
-  before_action :move_to_index, except: [:index,:show] 
   before_action :editor_is_correct_user, only: [:edit, :update]
+  before_action :find_good_recod, only[:show,:edit,:update]
 
   def index
     @goods = Good.includes(:user).order("created_at DESC")
@@ -21,15 +21,12 @@ class GoodsController < ApplicationController
   
 
   def show
-    @good = Good.find(params[:id])
   end
 
   def edit
-    @good = Good.find(params[:id])
   end
 
   def update
-    @good = Good.find(params[:id])
     if @good.update(good_params)
       redirect_to good_path
     else
@@ -45,15 +42,13 @@ class GoodsController < ApplicationController
        :price, :image).merge(user_id: current_user.id)
   end
 
-  def move_to_index
-    unless user_signed_in?
-      redirect_to new_user_session_path
-    end
-  end
-
   def editor_is_correct_user
     unless Good.find(params[:id]).user.id == current_user.id
       redirect_to root_path
     end
+  end
+
+  def find_good_recod
+    @good = Good.find(params[:id])
   end
 end
