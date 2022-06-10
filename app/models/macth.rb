76dde14bae1c3628bef7f82e005f653f441908b5
harukicode0 <71533263+harukicode0@users.noreply.github.com>
2022-06-10 16:47:26 +0900

    matchテーブルの作成
    This reverts commit e4bdfa65452c0db99fde03d1d0eb1813ac4cb18b.

--- app/models/macth.rb
+++
@@ -1,2 +0,0 @@
-class Macth < ApplicationRecord
-end
--- db/migrate/20220610074145_create_macths.rb
+++
@@ -1,9 +0,0 @@
-class CreateMacths < ActiveRecord::Migration[6.0]
-  def change
-    create_table :macths do |t|
-      t.references :user, foreign_key: true
-      t.references :good, foreign_key: true
-      t.timestamps
-    end
-  end
-end
