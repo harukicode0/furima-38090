daba25c66a860b4484be750609164ef78292bb8e
harukicode0 <71533263+harukicode0@users.noreply.github.com>
2022-06-10 18:19:59 +0900

    テーブル名がmacthとなっていたので、これをmatchに修正

--- db/migrate/20220610085314_create_matches.rb
+++ db/migrate/20220610085314_create_matches.rb
@@ -1,6 +1,6 @@
-class CreateMacths < ActiveRecord::Migration[6.0]
+class CreateMatches < ActiveRecord::Migration[6.0]
   def change
-    create_table :macths do |t|
+    create_table :matches do |t|
       t.references :user, foreign_key: true
       t.references :good, foreign_key: true
       t.timestamps
