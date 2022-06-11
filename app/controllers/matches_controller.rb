d37091c4107fcc9f9d544fd5900b4fd57bd8203a
harukicode0 <71533263+harukicode0@users.noreply.github.com>
2022-06-11 15:53:05 +0900

    matchコントローラー編集

--- app/controllers/matches_controller.rb
+++ app/controllers/matches_controller.rb
@@ -1,5 +1,19 @@
 class MatchesController < ApplicationController
   def index
     @good = Good.find(params[:good_id])
+    @match = Match.new
+  end
+
+  def create 
+    binding.pry
+    @match = Match.new(match_params)
+  end
+  
+  private
+
+  def match_params
+    params.require(:match).permit(:name, :description, :category_id, 
+      :condition_id, :delivery_load_id, :prefecture_id, :delivery_day_id,
+       :price, :image).merge(user_id: current_user.id)
   end
 end
