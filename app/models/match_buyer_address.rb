687d57fca0c4923979a4d1dd3d56fabe2136c135
harukicode0 <71533263+harukicode0@users.noreply.github.com>
2022-06-11 16:31:48 +0900

    form_objectのvalidates作成

--- app/models/match_buyer_address.rb
+++ app/models/match_buyer_address.rb
@@ -1,5 +1,18 @@
 class DonationAddress
   include ActiveModel::Model
-  attr_accessor :user_id, :good_id, :address_number, :prefecture_id,
+  attr_accessor :user_id, :good_id, :address_number, :prefecture,
   :city, :banti, :building_name, :phone_number,:match_id
+
+  address_number_regex = /\A[\d]{3}[-][\d]{4}\z/
+  phone_number_ten_or_eleven_regex = /\A[\d]{10,11}\z/
+
+  with_options presence :true do
+    validates :user_id
+    validates :good_id
+    validates :address_number,format: { with: address_number_regex, message: "can't be blank" }
+    validates :city
+    validates :phone_number,format:{with:phone_number_ten_or_eleven_regex, message: "can't be blank"}
+    validates :match_id
+  end
+  validates :prefecture, numericality: {other_than: 1, message: "can't be blank"}
 end
