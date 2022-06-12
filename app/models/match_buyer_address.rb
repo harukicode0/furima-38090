65bf9c9468979b49fd241d79820f784aaf5a9fc2
harukicode0 <71533263+harukicode0@users.noreply.github.com>
2022-06-12 22:12:38 +0900

    factoryの作成

--- app/models/match_buyer_address.rb
+++ app/models/match_buyer_address.rb
@@ -5,12 +5,13 @@ class MatchBuyerAddress
 
   address_number_regex = /\A[\d]{3}[-][\d]{4}\z/
   phone_number_ten_or_eleven_regex = /\A[\d]{10,11}\z/
+  check_name_kanji_kana_hiragana_REGEX = /\A[ぁ-んァ-ヶ一-龥々ー]+\z/
 
   with_options presence: true do
     validates :user_id
     validates :good_id
     validates :address_number,format: { with: address_number_regex, message: "は○○○-(半角ハイフン)○○○○の形で入力してください(○は数字)" }
-    validates :city
+    validates :city, format: {with: check_name_kanji_kana_hiragana_REGEX, message: "は全角入力でお願いします"}
     validates :phone_number,format:{with:phone_number_ten_or_eleven_regex, message: "は１０〜１１桁の数字で入力してください"}
     validates :token
   end
--- spec/factories/match_buyer_addresses.rb
+++ spec/factories/match_buyer_addresses.rb
@@ -1,5 +1,12 @@
 FactoryBot.define do
   factory :match_buyer_address do
-    
+    address = Gimei.address
+
+    address_number{Faker::Number.number(digits: 3).to_s + "-" + Faker::Number.number(digits: 4).to_s}
+    city{address.city.kanji}
+    banti{address.town.kanji}
+    building_name{Faker::Lorem.sentence}
+    phone_number{Faker::Number.number(digits: 10)}
+    token{"tok_abcdefghijk00000000000000000"}
   end
 end
