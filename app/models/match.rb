5b5fb5875314a0b77bda66fabfed5ad2b7864100
harukicode0 <71533263+harukicode0@users.noreply.github.com>
2022-06-12 15:57:30 +0900

    一意生制約の削除。理由はアソシエーションで既に一意性が保証されているため

--- app/models/match.rb
+++ app/models/match.rb
@@ -1,6 +1,4 @@
 class Match < ApplicationRecord
-  validates :good_id, uniqueness: true
-  
   belongs_to :good
   belongs_to :user 
   has_one :buyer_address
--- db/migrate/20220610085314_create_matches.rb
+++ db/migrate/20220610085314_create_matches.rb
@@ -2,7 +2,7 @@ class CreateMatches < ActiveRecord::Migration[6.0]
   def change
     create_table :matches do |t|
       t.references :user, foreign_key: true
-      t.references :good, foreign_key: true, index: { unique: true }
+      t.references :good, foreign_key: true
       t.timestamps
     end
   end
